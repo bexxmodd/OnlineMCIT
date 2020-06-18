14607f184dadb6fb9576a9389115472e89b5739e
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2020-06-18 11:28:52 -0400

    greets with personal message

---
+++ Discord/bots/welcomebot.py
@@ -0,0 +1,34 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+
+client = discord.Client()
+
+@client.event
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_member_join(member):
+    print("Recognized that " + member.name + " joined")
+    msg = f'Hello <@{member.id}>! Welcome to the UPenn Online MCIT Discord Server. Please head to the <#722617468622733385> and read the instructions to start using the server.'
+    channel = client.get_channel(722612886924427357)
+    await channel.send(msg)
+    print(member.name + " was greeted")
+
+
+client.run(TOKEN)
